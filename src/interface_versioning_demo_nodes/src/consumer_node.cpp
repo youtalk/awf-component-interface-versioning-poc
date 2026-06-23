0e4f14b3dfc75189c386f2616df335716b730cb0
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2026-06-22 20:09:03 -0700

    feat(demo): add provider and consumer demo nodes
    Signed-off-by: Yutaka Kondo <yutaka.kondo@youtalk.jp>

---
+++ src/interface_versioning_demo_nodes/src/consumer_node.cpp
@@ -0,0 +1,46 @@
+#include "autoware_component_interface_specs_poc/perception.hpp"
+#include "autoware_component_interface_utils_poc/node_adaptor.hpp"
+#include "autoware_component_interface_utils_poc/node_interface.hpp"
+
+#include <rclcpp/rclcpp.hpp>
+
+#include <autoware_perception_msgs/msg/predicted_objects.hpp>
+
+#include <cstdint>
+#include <memory>
+
+namespace specs = autoware::component_interface_specs_poc;
+namespace utils = autoware::component_interface_utils_poc;
+
+class ConsumerNode : public rclcpp::Node
+{
+public:
+  ConsumerNode() : Node("consumer")
+  {
+    const int consumer_major = declare_parameter<int>("consumer_major", 2);
+    iface_ = std::make_shared<utils::NodeInterface>();
+    utils::NodeAdaptor adaptor(this, iface_);
+    sub_ = adaptor.create_sub<specs::perception::ObjectRecognition>(
+      [this](autoware_perception_msgs::msg::PredictedObjects::SharedPtr) {
+        if (++count_ % 25 == 0) {
+          RCLCPP_INFO(get_logger(), "received %u messages", count_);
+        }
+      },
+      specs::accept_major(static_cast<std::uint16_t>(consumer_major)));
+    iface_->broadcast(this, "autoware");
+    RCLCPP_INFO(get_logger(), "consumer requires perception accept_major(%d)", consumer_major);
+  }
+
+private:
+  std::shared_ptr<utils::NodeInterface> iface_;
+  rclcpp::Subscription<autoware_perception_msgs::msg::PredictedObjects>::SharedPtr sub_;
+  std::uint32_t count_{0};
+};
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<ConsumerNode>());
+  rclcpp::shutdown();
+  return 0;
+}
---
+++ src/interface_versioning_demo_nodes/src/provider_node.cpp
@@ -0,0 +1,44 @@
+#include "autoware_component_interface_specs_poc/perception.hpp"
+#include "autoware_component_interface_utils_poc/node_adaptor.hpp"
+#include "autoware_component_interface_utils_poc/node_interface.hpp"
+
+#include <rclcpp/rclcpp.hpp>
+
+#include <autoware_perception_msgs/msg/predicted_objects.hpp>
+
+#include <chrono>
+#include <memory>
+
+namespace specs = autoware::component_interface_specs_poc;
+namespace utils = autoware::component_interface_utils_poc;
+
+class ProviderNode : public rclcpp::Node
+{
+public:
+  ProviderNode() : Node("provider")
+  {
+    iface_ = std::make_shared<utils::NodeInterface>();
+    utils::NodeAdaptor adaptor(this, iface_);
+    pub_ = adaptor.create_pub<specs::perception::ObjectRecognition>();
+    iface_->broadcast(this, "autoware");
+    timer_ = create_wall_timer(
+      std::chrono::milliseconds(200), [this]() {
+        autoware_perception_msgs::msg::PredictedObjects msg;
+        msg.header.stamp = now();
+        pub_->publish(msg);
+      });
+  }
+
+private:
+  std::shared_ptr<utils::NodeInterface> iface_;
+  rclcpp::Publisher<autoware_perception_msgs::msg::PredictedObjects>::SharedPtr pub_;
+  rclcpp::TimerBase::SharedPtr timer_;
+};
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<ProviderNode>());
+  rclcpp::shutdown();
+  return 0;
+}
